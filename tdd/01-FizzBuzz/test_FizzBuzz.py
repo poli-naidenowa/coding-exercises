897791e17ba54e60ace772f4e6049121eb7a2c7b
Paulina Veskova <76879205+poli-naidenowa@users.noreply.github.com>
2023-11-01 10:09:37 +0200

    Add tests test_FizzBuzz.py

--- tdd/01-FizzBuzz/test_FizzBuzz.py
+++ tdd/01-FizzBuzz/test_FizzBuzz.py
@@ -3,14 +3,43 @@ import FizzBuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
-    # 1. Write a `fizzBuzz` method that accepts a number as input and returns it as a String.
-    def test_is_input_is_number(self):
 
-        number = FizzBuzz.readInput()
-        result = number.isnumeric()
-        expected = True
+    def test_is_input_is_number_gw(self):
+        """ 
+        1. Write a `fizzBuzz` method that accepts a 
+        number as input and returns it as a String.
+        """
+        number = 2
+        result = FizzBuzz.fizzBuzz(number)
 
-        self.assertEqual(result, expected)
-        
+        self.assertEqual(result, str(number))
+    
+    def test_is_Fizz(self):
+        """
+        2. For multiples of three return “Fizz” instead of the number
+        """
+        number = 3
+        result = FizzBuzz.fizzBuzz(number)
+
+        self.assertEqual(result, "Fizz")
+
+    def test_is_Buzz(self):
+        """
+        3. For the multiples of five return “Buzz”
+        """
+        number = 5
+        result = FizzBuzz.fizzBuzz(number)
+
+        self.assertEqual(result, "Buzz")
+
+    def test_is_FizzBuzz(self):
+        """
+        4. For numbers that are multiples of both three and five return “FizzBuzz”
+        """
+        number = 15
+        result = FizzBuzz.fizzBuzz(number)
+
+        self.assertEqual(result, "FizzBuzz")
+
 if __name__ == '__main__': 
     unittest.main() 
